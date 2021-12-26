4aa6f1427d9198c58213af6453f0e255dcf40ee5
Jon Tsikniadopoulos <63033921+jtsiknias@users.noreply.github.com>
2021-12-25 21:03:52 -0800

    add search and loading progress bar

--- components/LargeCard.js
+++ components/LargeCard.js
@@ -14,7 +14,7 @@ function LargeCard({ img, title, description, buttonText }) {
       <div className="absolute top-32 left-12">
         <h3 className="text-4xl mb-3 w-64">{title}</h3>
         <p>{description}</p>
-        <button className="text-sm text-white bg-gray-900 mt-5 px-4 py-2 rounded-lg">
+        <button className="text-white bg-gray-900 hover:text-gray-900 border-2 border-gray-900 hover:bg-white mt-5 px-8 py-4 rounded-lg transition duration-150 hover:ease-in-out">
           {buttonText}
         </button>
       </div>
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import ProgressBar from "@badrap/bar-of-progress";
+import Router from "next/router";
+
+const progress = new ProgressBar({
+  size: 6,
+  color: "#FE595E",
+  className: "z-50",
+  delay: 100,
+});
+
+// when you change Route, it dispatches an action. Tapping into it here
+// Progress bar will start loading at the point the route started to change
+Router.events.on("routeChangeStart", progress.start);
+// Finish point
+Router.events.on("routeChangeComplete", progress.finish);
+Router.events.on("routeChangeError", progress.finish);
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
